5937da7ba3fa6aaf0ade0ed24ab15cfe27cfa2f5
root <root@localhost>
2017-06-15 14:39:49 -0600

    trying it from awd ide on tablet

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 		<title>I completed the prework</title>
 	</head>
 	<body>
-		<p>I completed the prework!...again...and again...and  a third time</p>
+		<p>I completed the prework!...again...and again...and  a third time...trying it from awd ide on tablet</p>
 	</body>
 </html>
