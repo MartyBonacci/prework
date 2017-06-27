f954ad43c0b37754bae81efd1be27121eb0b36bc
Marty Bonacci <martybonacci@gmail.com>
2017-06-27 15:20:44 -0600

    iso 8859

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="iso-8859-11">
+		<meta charset="ISO-8859-1">
 		<title>I completed the prework</title>
 	</head>
 	<body>
 		<h1>ELEMENT 14</h1>
 		<php></php>
+		<p>Japanese example: ご飯が熱い。</p>
 		<p>I completed the prework!...again...and again...and  a third time...trying it from awd ide on tablet</p>
 	</body>
 </html>
